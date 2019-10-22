8d9e68ad02338fb71bed03c63506723f9d5d786b
ETHAN DEAN <ETHDEAN@uat.edu>
2019-10-22 16:06:16 -0700

    Added code for ragdolls

---
+++ GPE340/Assets/Scripts/AIController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIController : MonoBehaviour
+{
+    public Transform target;
+    public Animator anim;
+    public NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target.position);
+
+        Vector3 desiredMovement = agent.desiredVelocity;
+        desiredMovement = transform.InverseTransformDirection(desiredMovement);
+        anim.SetFloat("Horizontal", desiredMovement.x);
+        anim.SetFloat("Vertical", desiredMovement.z);
+    }
+
+    private void OnAnimatorMove()
+    {
+        agent.velocity = anim.velocity;
+        Debug.Log(anim.velocity);
+    } 
+}
---
+++ GPE340/Assets/Scripts/Ragdoll Controller.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollController : MonoBehaviour
+{
+    private CapsuleCollider mainCapsuleCollider;
+    private Rigidbody mainRigidbody;
+    private Animator animator;
+    private Rigidbody[] ragdollRigidBodies;
+    private CapsuleCollider[] ragdollColliders;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCapsuleCollider = GetComponent<CapsuleCollider>();
+        mainRigidbody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+
+        ragdollRigidBodies = GetComponentsInChildren<Rigidbody>();
+        ragdollColliders = GetComponentsInChildren<CapsuleCollider>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+           
+
+        }
+    }
+
+    void toggleragdoll(bool isOn)
+    {
+        if (isOn)
+        {
+            for(int i = 0; i < ragdollColliders.Length; i++)
+            {
+                ragdollColliders[i].enabled = true;
+            }
+            // turn on ragdoll
+            // turn off main elements
+            mainCapsuleCollider.enabled = false;
+            animator.enabled = false;
+            mainRigidbody.isKinematic = true;
+        }
+        else
+        {
+            // turn off main elements
+            // turn on ragdoll
+            mainCapsuleCollider.enabled = true;
+            animator.enabled = true;
+            mainRigidbody.isKinematic = false;
+        }
+    }
+
+}
